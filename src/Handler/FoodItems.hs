6052d08e1570572684b30393e7ec141175b8c502
OrthogonalScribe <OrthogonalScribe@gmail.com>
2024-04-21 22:28:39 +0200

    postFoodItemsR working

--- src/Handler/FoodItems.hs
+++ src/Handler/FoodItems.hs
@@ -8,29 +8,58 @@ module Handler.FoodItems where
 
 import Import
 
+foodItemForm :: UserId -> Form FoodItem
+foodItemForm userId = renderDivs $ FoodItem
+    <$> pure userId
+    <*> areq textField "Name" Nothing
+    <*> areq doubleField "Kcal/100g" Nothing
+    <*> areq doubleField "carbs/100g" Nothing
+    <*> areq doubleField "protein/100g" Nothing
+    <*> areq doubleField "fat/100g" Nothing
+
+postFoodItemsR :: Handler Html
+postFoodItemsR = do
+    Entity userId _user <- requireAuth
+    ((result, _widget), _enctype) <- runFormPost $ foodItemForm userId
+    case result of
+        FormSuccess foodItem -> do
+            _foodItemId <- runDB $ insert foodItem
+            setMessage [shamlet|Food item #{foodItemName foodItem} created|]
+            redirect FoodItemsR
+        _ -> defaultLayout $ do
+            setMessage "Invalid data entered for new food item!"
+            redirect FoodItemsR
+
 getFoodItemsR :: Handler Html
 getFoodItemsR = do
-    (_, user) <- requireAuthPair
+    Entity userId user <- requireAuth
+    ((_result, widget), enctype) <- runFormPost $ foodItemForm userId
     entries <- runDB $ selectList [] [Asc FoodItemName]
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s food items"
-        toWidgetBody
-            [hamlet|
-                <div .ui.container>
+        [whamlet|
+            <div .ui.container>
 
-                    <h1>
-                        Access granted!
+                <h1>
+                    Access granted!
 
-                    <p>
-                        This page is protected and access is allowed only for authenticated users.
+                <p>
+                    This page is protected and access is allowed only for authenticated users.
 
-                    <p>
-                        MAGWEG-DEBUG: userIdent = <span class="username">#{userIdent user}</span>
+                <p>
+                    MAGWEG-DEBUG: userIdent = <span class="username">#{userIdent user}</span>
 
-                    $if null entries
-                        <p>No food items found.
-                    $else
-                        <ul>
-                            $forall Entity _foodItemId item <- entries
-                                <li>#{show item}
-            |]
+                <h2>Add new food item
+                <form method=post action=@{FoodItemsR} enctype=#{enctype}>
+                    ^{widget}
+                    <input type=submit>
+
+                <h2>Food items
+                <p>
+                $if null entries
+                    <p>No food items found.
+                $else
+                    <ul>
+                        $forall Entity _foodItemId item <- entries
+                            <li>#{show item}
+        |]
